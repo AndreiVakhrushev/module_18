fcc6a282c0e0f89df59f22e7228cc48fa4d7a857
AndreiVakhrushev <vakhroushev@bk.ru>
2025-01-10 23:02:25 +0300

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task2.views import func_template, ClassTemplate
+from task3.views import platform, games, card
 from django.views.generic import TemplateView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('func/', func_template),
     path('class/', ClassTemplate.as_view()),
+    path('platform/', platform),
+    path('platform/games/', games),
+    path('platform/card/', card),
 ]
